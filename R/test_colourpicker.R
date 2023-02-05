e1ce0b7e26c5d986b785c551010709074293a780
emanuelhuber <emanuel.huber@tranquille.ch>
2023-02-05 16:09:35 +0100

    first commit

---
+++ R/test_colourpicker.R
@@ -0,0 +1,29 @@
+
+
+
+#' test_colourpicker
+#'
+#' This is a shiny project
+#'
+#' @import graphics
+#' @import shiny
+#' @import colourpicker
+"_PACKAGE"
+#> [1] "_PACKAGE"
+
+
+
+#' Lauch the test_colourpicker  app
+#'
+#' Lauch the test_colourpicker  app!
+#'
+#' @param ... Arguments to be passed to the function
+#' @export
+#'
+#' @return shiny application object
+testcolourpicker <- function(...) {
+  shinyApp(ui = shinyAppUI, server = shinyAppServer, ...)
+}
+
+
+
